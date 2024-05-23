4de8a4e3f6d4e4ff664154e7e0a5512a255d3812
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-05-23 15:22:34 +0100

    AirBnB_Clone

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""This module defines a class `user`"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+'''Defines a user'''
+email = ""
+password = ""
+first_name = ""
+last_name = ""
